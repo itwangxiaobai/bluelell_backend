87c095b52858955b36110201323fc17e98f1ecf0
itwangxiaobai <Ms123!@#>
2024-01-06 20:59:50 +0800

    增加根据社区id查询帖子接口

--- controller/post.go
+++ controller/post.go
@@ -72,6 +72,17 @@ func GetPostListHandler(c *gin.Context) {
 }
 
 // GetPostListHandler2 根据发帖时间或帖子分数获取帖子列表处理函数
+// GetPostListHandler2 升级版帖子列表接口
+// @Summary 升级版帖子列表接口
+// @Description 可按社区按时间或分数排序查询帖子列表接口
+// @Tags 帖子相关接口(api分组展示使用的)
+// @Accept application/json
+// @Produce application/json
+// @Param Authorization header string true "Bearer JWT"
+// @Param object query models.ParamPostList false "查询参数"
+// @Security ApiKeyAuth
+// @Success 200 {object} _ResponsePostList
+// @Router /posts2 [get]
 func GetPostListHandler2(c *gin.Context) {
 	// GET请求参数（query string）：api/v1/posts2?page=1&size=10&order=time
 	// 初始化结构体时指定初始参数
--- models/params.go
+++ models/params.go
@@ -27,8 +27,8 @@ type ParamVoteData struct {
 }
 
 type ParamPostList struct {
-	CommunityID int64  `json:"community_id" form:"community_id"` // 可以为空
-	Page        int64  `json:"page" form:"page"`                 // 页码
-	Size        int64  `json:"size" form:"size"`                 // 每页的数量
-	Order       string `json:"order" form:"order"`               // 排序依据
+	CommunityID int64  `json:"community_id" form:"community_id"`   // 可以为空
+	Page        int64  `json:"page" form:"page" example:"1"`       // 页码
+	Size        int64  `json:"size" form:"size" example:"10"`      // 每页的数量
+	Order       string `json:"order" form:"order" example:"score"` // 排序依据
 }
